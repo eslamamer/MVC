7ae3169073a85c5ed06d09528916c3ba7695e389
eslamamer <eslam.amer@hotmail.com>
2026-07-28 22:20:34 +0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mvc/elframework',
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mvc/elframework' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'php/elframework' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'f80f46282417ac22dfe5bd8008e65fe8b1548ce6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php/elframework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
